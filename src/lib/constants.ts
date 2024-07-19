8415e8c12142d0262db6d9c9adbac668a6e0378b
Egehan KAHRAMAN <vastseaoffical0@outlook.com>
2024-07-19 04:06:08 +0300

    Fixed links on typescript file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const LINKS = {
   github: "https://github.com/VastSea0",
   instagram: "https://www.instagram.com/99laz_can/",
   instagram_two: "https://www.instagram.com/99laz.can/",
-  discord: "https://discord/users/609122114003533824",
+  discord: "https://discord.com/users/609122114003533824",
  
 };
 
